8aaaee08ba09ca5096799b6c5a3d116bbf99972c
hayashi311 <hayashi311@gmail.com>
2015-12-24 18:35:25 +0900

    Initial Commit

---
+++ UIResponderBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIResponderBug
+//
+//  Created by hayashi311 on 12/24/15.
+//  Copyright © 2015 hayashi311. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIResponderBug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIResponderBug
+//
+//  Created by hayashi311 on 12/24/15.
+//  Copyright © 2015 hayashi311. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
